aea32c522d0cee8f7d637262e81384086c374041
ita-design <ita@tolotravel.com>
2025-10-07 12:53:56 +0430

    create a about page

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 console.log("It is Mohammad Manosur Latifi");
-console.log("In this section you can write anything that you want")
-console.log("Now we created a New Branch by the name of Login")
+console.log("In this section you can write anything that you want");
+console.log("Now we created a New Branch by the name of Login");
+console.log("In this section we create a new Branch by the name of about");
+console.log("Now you can wirte somethings in about part or section, about yourself");
